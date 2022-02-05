e95db18f62c4553bb06b04451b6b7116ec7f7df3
Jason Greenia <jason.greenia@gmail.com>
2022-02-04 22:37:38 -0800

    added function for crypto compare, since technical analysis API had cors enabled

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,43 @@
-//var coinCapApiKey = "077197b0-ec28-42d9-a263-4d69a7e393ea";
+var cryptoCompareApiKey = "c3d24075ea2af90c9c9419267e59744738dc365a925003c0232e1ebef14a98b1";
+
+var getCryptoCompareDataBySymbol = function (symbol) {
+
+    var cryptoCompareURL = "https://min-api.cryptocompare.com/data/v2/news/?lang=EN&categories=" + symbol + "&api_key=" + cryptoCompareApiKey
+    console.log("Fetching: " + cryptoCompareURL);
+
+    fetch(cryptoCompareURL)
+
+        // Check if response is OK and if it is, load response as json
+        .then(response => {
+            if (response.ok) {
+                return response.json()
+            } else {
+                return Promise.reject("API did not return an OK response.");
+            }
+        })
+
+        // check if we recieved data back and if we did, display it to user
+        .then(cryptoCompareData => {
+            if (cryptoCompareData != "" && cryptoCompareData != null) {
+                return displayCryptoCompareData(cryptoCompareData);
+            }
+        })
+
+        // if we encounter errors above, this catch block will run
+        .catch(function (error) {
+            console.log(error);
+        });
+}
 
 var getCoinCapDataBySymbol = function (symbol) {
-    
+
     var coinCapAssetsURL = "https://api.coincap.io/v2/assets/" + symbol
     //CoinCap documentation reccomends to use these options: https://docs.coincap.io/#d4bac290-230a-48c6-a8eb-6804b2d137f3
-    var requestOptions = { 
+    var requestOptions = {
         method: 'GET',
         redirect: 'follow'
     };
-    
+
     console.log("Fetching: " + coinCapAssetsURL);
     fetch(coinCapAssetsURL, requestOptions)
 
@@ -32,7 +61,10 @@ var getCoinCapDataBySymbol = function (symbol) {
         .catch(function (error) {
             console.log(error);
         });
+}
 
+var displayCryptoCompareData = function (cryptoCompareData) {
+    console.log(cryptoCompareData)
 }
 
 var displayCoinCapData = function (coinCapData) {
@@ -48,3 +80,4 @@ var displayCoinCapData = function (coinCapData) {
 }
 
 getCoinCapDataBySymbol("bitcoin");
+getCryptoCompareDataBySymbol("BTC")
