5465e158af1a8ee015b4167d788b9964a2ba3cd2
Sally <sallywing123@gmail.com>
2022-02-08 20:25:59 -0800

    display current date and time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var cryptoCompareApiKey = "c3d24075ea2af90c9c9419267e59744738dc365a925003c0232e1ebef14a98b1";
 var coinLayerApiKey = "badc6c35cf23ca80b1c1af4c8eda2e1d";
 
@@ -50,92 +49,52 @@ var getCoinLayerDataBySymbol = function (symbol) {
         // check if we recieved data back and if we did, display it to user
         .then(coinLayerData => {
             if (coinLayerData != "" && coinLayerData != null) {
-                return displayCoinLayerData(coinLayerData);
+                
+            // var timeStamp = coinLayerData.timestamp;
+            // var currentDate = new Date (timeStamp);
+            // var currentTime = currentDate.getTime();
+            // var month = currentDate.getMonth();
+            // var year = currentDate.getFullYear();
+            // var hour = currentDate.getHours();
+            // var minute = currentDate.getMinutes();
+            
+
+            return displayCoinLayerData(coinLayerData);
+
             }
         })
+
+
 
         // if we encounter errors above, this catch block will run
         .catch(function (error) {
             console.log(error);
         });
+
+
+
+
 }
 
 var displayCryptoCompareData = function (cryptoCompareData) {
     console.log(cryptoCompareData)
 }
 
+
 var displayCoinLayerData = function (coinLayerData) {
     console.log(coinLayerData)
 
+    var dateAndTime = moment().format('MMMM Do YYYY, h:mm:ss a');   
+    console.log(dateAndTime)
+
+    //display date and time 
+    $('.time-date').append(dateAndTime)
 }
+
+
+
+
+
 
 getCoinLayerDataBySymbol("BTC");
 getCryptoCompareDataBySymbol("BTC")
-=======
-symbols = ['BTC','ETH','LTC','XRP','ADA','CRO']
-
-for (var i = 0; i < symbols.length ; i++) {
-    function coinLayerFetch(){
-        fetch('http://api.coinlayer.com/api/live?access_key=badc6c35cf23ca80b1c1af4c8eda2e1d&Symbols='+symbols[i])
-            .then(response => response.json()) 
-            .then(data => {
-                console.log(data)})
-
-
-        
-// function displayData (symbol,price){
-//     console.log(symbol,price)
-    // var cryptoValue = document.getElementById('cryptoValue')
-    // cryptoValue.appendChild(rate)    
-}
-
-coinLayerFetch()
-
-// News: cryptoCompare
-
-var cryptoCompareApiKey = "c3d24075ea2af90c9c9419267e59744738dc365a925003c0232e1ebef14a98b1";
-
-var CryptoCompareData = function () {
-
-    var cryptoCompareURL = "https://min-api.cryptocompare.com/data/v2/news/?lang=EN&categories=" + symbols[i] + "&api_key=" + cryptoCompareApiKey
-    fetch(cryptoCompareURL)
-        .then(response => response.json()) 
-        .then(data => {
-            console.log(data)
-        
-        })
-    }
-CryptoCompareData()
-
-
-
-// // Coinlayer, gives latest crypto rates for all available or a specific set of cryptocurrencies. 
-// function coinLayerFetch(){
-//     fetch('http://api.coinlayer.com/api/live?access_key=40b565543bfa215722cf41a447afab32&Symbols=BTC,ETH,LTC,XRP,ADA,CRO')
-//         .then(response => response.json()) 
-//         .then(data => {
-//         console.log(data) 
-//         var rate = data.rates
-//         console.log(rate) 
-//     })
-//   }
-// Append Coin Layer Data 
-
-// var dropDownItems = document.getElementsByClassName('dropdown-item')
-// for (var i = 0; i < dropDownItems.length ; i++) {
-//     var rawSymbol = dropDownItems[i].textContent
-//     rawSymbol = rawSymbol.trim().split('-')
-//     var symbol = rawSymbol[0];
-//     console.log(symbol)
-
-//     var cryptoValue = document.getElementById('cryptoValue')
-//     cryptoValue.appendChild(rate)
-// }
-
-
-
-
-
-
-}
->>>>>>> SallyJava
