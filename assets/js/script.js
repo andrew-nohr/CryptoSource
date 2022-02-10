a3ae112fe69c0fbb0b342318b2121b00e6eff039
Jason Greenia <jason.greenia@gmail.com>
2022-02-09 18:19:06 -0800

    fix the unicode issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,12 +7,14 @@ var dropdown = document.querySelector('.dropdown');
 var getCryptoCompareDataBySymbol = function (symbol) {
 
     var cryptoCompareURL = "https://min-api.cryptocompare.com/data/v2/news/?lang=EN&categories=" + symbol + "&api_key=" + cryptoCompareApiKey
-
+    var myHeaders = new Headers();
+    //myHeaders.append('Content-Type','text/plain; charset=UTF-8');
     fetch(cryptoCompareURL)
 
         // Check if response is OK and if it is, load response as json
         .then(response => {
             if (response.ok) {
+                //response.setContentType = "text/html;charset=ISO-8859-1";
                 return response.json()
             } else {
                 return Promise.reject("API did not return an OK response.");
@@ -113,7 +115,7 @@ var displayCryptoCompareData = function (cryptoCompareData) {
 
         //Set the card description from the API call data
         var cardDescription = document.createElement("p");
-        cardDescription.textContent = cryptoCompareData.Data[i].body;
+        cardDescription.textContent = cleanString(cryptoCompareData.Data[i].body);
         content.append(cardDescription);
 
         //Set the read button link target from the API call
@@ -152,6 +154,16 @@ var toggleDropdown = function (event){
             getCryptoCompareDataBySymbol("CRO")
         }
     }
+}
+
+// API sends back unicode character codes in the response string
+// This neat function will convert the unicode characters back into the characters they represent
+// https://stackoverflow.com/questions/30903627/replace-unicode-characters-with-characters-javascript
+var cleanString = function (str) {
+    return str.replace(/&#([0-9]{1,4});/gi, function(match, numStr) {
+        var num = parseInt(numStr, 10); // read num as normal number
+        return String.fromCharCode(num);
+    });
 }
 
 
